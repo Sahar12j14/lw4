ef7484cf3184c0ea6a9966ef46333eb0752134a8
Sahar12j14 <71217328+Sahar12j14@users.noreply.github.com>
2021-03-04 18:19:08 +0300

    update

--- src/js/index.js
+++ src/js/index.js
@@ -1,133 +1,127 @@
 import '../css/style.css';
 
-function fieldValidationName(element) {
+function ValidationName(element) {
   let checkErorr = true;
   if (!isFilled(element.value)) {
-  checkErorr = false;
-  element.style['boxShadow'] = 'inset 0 -1px 0 red';
-  return -1;
+    checkErorr = false;
+    element.classList.add('form-registration__input__error');
+    return {
+      errCode: -1,
+      errMessage: 'Expected input name'
+    };
   }
   if (isNumber(element.value)) {
-  checkErorr = false;
-  element.style['boxShadow'] = 'inset 0 -1px 0 red';
-  return 1;
+    checkErorr = false;
+    element.classList.add('form-registration__input__error');
+    return {
+      errCode: 1,
+      errMessage: 'Name must not contain numbers'
+    };
   }
   if (!checkErorr) {
   } else {
-  element.style['boxShadow'] = 'inset 0 -1px 0 rgba(199, 199, 199, 0.3)';
-    return 0;
+    element.classList.remove('form-registration__input__error');
+    return {
+      errCode: 0
+    };
   }
   console.log(element.value.length);
+}
+function emailValidation(email) {
+  let checkErorr = true;
+  if (!isFilled(email.value)) {
+    checkErorr = false;
+    email.classList.add('form-registration__input__error');
+    return {
+      errCode: -1,
+      errMessage: 'Expected input email'
+    };
   }
-  function emailValidation(email) {
-    let checkErorr = true;
-    if (!isFilled(email.value)) {
-      checkErorr = false;
-      email.style['boxShadow'] = 'inset 0 -1px 0 red';
-      return -1;
-    }
-    if (!isEmail(email.value)) {
-      checkErorr = false;
-      email.style['boxShadow'] = 'inset 0 -1px 0 red';
-      return 1;
-    }
-    if (!checkErorr) {
-    } else {
-    email.style['boxShadow'] = 'inset 0 -1px 0 rgba(199, 199, 199, 0.3)';
-      return 0;
-    }
+  if (!isEmail(email.value)) {
+    checkErorr = false;
+    email.classList.add('form-registration__input__error');
+    return {
+      errCode: 1,
+      errMessage: 'Incorrect email format'
+    };
   }
-  function passwordValidation(password) {
-    let checkErorr = true;
-    if (!isFilled(password.value)) {
-      checkErorr = false;
-      password.style['boxShadow'] = 'inset 0 -1px 0 red';
-      return -1;
-    }
-    if (password.value.length < 6) {
-      checkErorr = false;
-      password.style['boxShadow'] = 'inset 0 -1px 0 red';
-      return 1;
-    }
-    if (!checkErorr) {
-    } else {
-    password.style['boxShadow'] = 'inset 0 -1px 0 rgba(199, 199, 199, 0.3)';
-      return 0;
-    }
+  if (!checkErorr) {
+  } else {
+    email.classList.remove('form-registration__input__error');
+    return {
+      errCode: 0
+    };
   }
-  function isFilled(element) {
-    if (element.length <= 0) {
-      return false;
-    }
+}
+function passwordValidation(password) {
+  let checkErorr = true;
+  if (!isFilled(password.value)) {
+    checkErorr = false;
+    password.classList.add('form-registration__input__error');
+    return {
+      errCode: 1,
+      errMessage: 'Expected input password '
+    };
+  }
+  if (password.value.length < 6) {
+    checkErorr = false;
+    password.classList.add('form-registration__input__error');
+    return {
+      errCode: -1,
+      errMessage: 'Password must be more than 6 characters'
+    };
+  }
+  if (!checkErorr) {
+  } else {
+    password.classList.remove('form-registration__input__error');
+    return {
+      errCode: 0
+    };
+  }
+}
+function isFilled(element) {
+  if (element.length <= 0) {
+    return false;
+  }
+  return true;
+}
+function isNumber(element) {
+  if (element.match(/[0-9]+/) !== null) {
     return true;
   }
-  function isNumber(element) {
-    if (element.match(/[0-9]+/) !== null) {
-      return true;
-    }
-    return false;
+  return false;
+}
+function isEmail(email) {
+  console.log(email);
+  if (email.match(/^[\w-.]+@[\w-]+.[a-z]{2,4}$/) !== null) {
+    return true;
   }
-  function isEmail(email) {
-    console.log(email);
-    if (email.match(/^[\w-.]+@[\w-]+.[a-z]{2,4}$/) !== null) {
-      return true;
-    }
-    return false;
+  return false;
+}
+var registrationButton = document.querySelector('#form-registration__button');
+registrationButton.onclick = function () {
+  const name = document.querySelector('#name');
+  const lastName = document.querySelector('#last-name');
+  const email = document.querySelector('#email');
+  const password = document.querySelector('#password');
+  if (ValidationName(name).errCode !== 0) {
+    document.getElementById('form-registration__item__error-name').innerHTML=ValidationName(name).errMessage;
+  } else {
+    document.getElementById('form-registration__item__error-name').innerHTML='';
   }
-  
-  var registrationButton = document.querySelector('#form-registration__button');
-  
-  registrationButton.onclick = function () {
-    let name = document.querySelector('#name');
-    let lastName = document.querySelector('#last-name');
-    let email = document.querySelector('#email');
-    let password = document.querySelector('#password');
-    if(fieldValidationName(name) === 0) {
-    document.getElementById('form-registration__item__error-name__empty').style.visibility = "hidden";
-    document.getElementById('form-registration__item__error-name__input').style.visibility = "hidden";
-    }
-    else if(fieldValidationName(name) === 1) {
-      document.getElementById('form-registration__item__error-name__empty').style.visibility = "hidden";
-      document.getElementById('form-registration__item__error-name__input').style.visibility = "visible";
-    }
-    else{
-      document.getElementById('form-registration__item__error-name__empty').style.visibility = "visible"; 
-      document.getElementById('form-registration__item__error-name__input').style.visibility = "hidden";     
-    }
-    if(fieldValidationName(lastName) === 0) {
-      document.getElementById('form-registration__item__error-last-name__empty').style.visibility = "hidden";
-      document.getElementById('form-registration__item__error-last-name__input').style.visibility = "hidden";
-      }
-      else if(fieldValidationName(lastName) === 1) {
-        document.getElementById('form-registration__item__error-last-name__empty').style.visibility = "hidden";
-        document.getElementById('form-registration__item__error-last-name__input').style.visibility = "visible";
-      }
-      else{
-        document.getElementById('form-registration__item__error-last-name__empty').style.visibility = "visible"; 
-        document.getElementById('form-registration__item__error-last-name__input').style.visibility = "hidden";     
-      }
-      if(emailValidation(email) === 0) {
-        document.getElementById('form-registration__item__error-email__empty').style.visibility = "hidden";
-        document.getElementById('form-registration__item__error-email__input').style.visibility = "hidden";
-        }
-        else if(emailValidation(email) === 1) {
-          document.getElementById('form-registration__item__error-email__empty').style.visibility = "hidden";
-          document.getElementById('form-registration__item__error-email__input').style.visibility = "visible";
-        }
-        else{
-          document.getElementById('form-registration__item__error-email__empty').style.visibility = "visible"; 
-          document.getElementById('form-registration__item__error-email__input').style.visibility = "hidden";     
-        }
-        if(passwordValidation(password) === 0) {
-          document.getElementById('form-registration__item__error-password__empty').style.visibility = "hidden";
-          document.getElementById('form-registration__item__error-password__input').style.visibility = "hidden";
-          }
-          else if(passwordValidation(password) === 1) {
-            document.getElementById('form-registration__item__error-password__empty').style.visibility = "hidden";
-            document.getElementById('form-registration__item__error-password__input').style.visibility = "visible";
-          }
-          else{
-            document.getElementById('form-registration__item__error-password__empty').style.visibility = "visible"; 
-            document.getElementById('form-registration__item__error-password__input').style.visibility = "hidden";     
-          }
-  };
+  if (ValidationName(lastName).errCode !== 0) {
+    document.getElementById('form-registration__item__error-last-name').innerHTML=ValidationName(lastName).errMessage;
+  } else {
+    document.getElementById('form-registration__item__error-last-name').innerHTML='';
+  }
+  if (emailValidation(email).errCode !== 0) {
+    document.getElementById('form-registration__item__error-email').innerHTML=emailValidation(email).errMessage;
+  } else {
+    document.getElementById('form-registration__item__error-email').innerHTML='';
+  }
+  if (passwordValidation(password).errCode !== 0) {
+    document.getElementById('form-registration__item__error-password').innerHTML=passwordValidation(password).errMessage;
+  } else {
+    document.getElementById('form-registration__item__error-password').innerHTML='';
+  }
+};
